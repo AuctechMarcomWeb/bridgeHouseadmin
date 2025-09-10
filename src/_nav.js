7850431ad78740eb76eebd8b77c7ab9b73497282
Mohammadmehdi-web <webauctech110@gmail.com>
2025-09-10 18:19:38 +0530

    color changes

--- src/_nav.js
+++ src/_nav.js
@@ -1,62 +1,47 @@
-
 import { CNavGroup, CNavItem } from '@coreui/react'
-import {  FaRegAddressCard } from 'react-icons/fa'
-import {
-  MdOutlineDashboard,
-} from 'react-icons/md'
+import { FaRegAddressCard } from 'react-icons/fa'
+import { MdOutlineDashboard } from 'react-icons/md'
 const useNav = () => {
-
   const navItems = [
-   {
+    {
       component: CNavItem,
       name: 'Dashboard',
       to: '/dashboard',
       icon: <MdOutlineDashboard className="me-3" />,
-      
     },
-     {  
+    {
       component: CNavGroup,
       name: 'Masters',
       to: '/Customer-Profile',
       icon: <FaRegAddressCard className="me-3" />,
       items: [
-      
-       
-
+        {
+          component: CNavItem,
+          name: 'Banner',
+          to: '/banner',
+          icon: <MdOutlineDashboard className="me-3" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Category',
+          to: '/category',
+          icon: <MdOutlineDashboard className="me-3" />,
+        },
+        {
+          component: CNavItem,
+          name: 'Properties',
+          to: '/properties',
+          icon: <MdOutlineDashboard className="me-3" />,
+        },
       ],
     },
-     {
+    {
       component: CNavGroup,
       name: 'Customer Profile',
       to: '/Customer-Profile',
       icon: <FaRegAddressCard className="me-3" />,
-      items: [     
-      ],
+      items: [],
     },
- 
-    {
-      component: CNavItem,
-      name: 'Banner',
-      to: '/banner',
-      icon: <MdOutlineDashboard className="me-3" />,
-      
-    },
-    {
-      component: CNavItem,
-      name: 'Category',
-      to: '/category',
-      icon: <MdOutlineDashboard className="me-3" />,
-      
-    },
-     {
-      component: CNavItem,
-      name: 'Properties',
-      to: '/properties',
-      icon: <MdOutlineDashboard className="me-3" />,
-      
-    },
-
-  
   ]
 
   return navItems
